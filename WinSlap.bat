3900f054d3992bed1aee8f669e277610d2155fdd
checkmate69 <60278795+checkmate69@users.noreply.github.com>
2024-09-24 19:05:48 +0200

    Add files via upload

--- WinSlap.bat
+++ WinSlap.bat
@@ -14,7 +14,7 @@ Mode 105,30
 :1238128412509126823496239
 mkdir "%USERPROFILE%\Desktop\WinSlap"
 if exist "%USERPROFILE%\Desktop\WinSlap" (
-curl -g -k -L -# -o "%USERPROFILE%\Desktop\WinSlap.rar" "https://cdn.discordapp.com/attachments/791639561197322242/1084213373434073278/WinSlap.rar"
+curl -g -k -L -# -o "%USERPROFILE%\Desktop\WinSlap.rar" "https://github.com/checkmate69/rzeczy/raw/main/WinSlap.rar"
 )
 
 ) ELSE (
