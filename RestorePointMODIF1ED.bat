9e5efad5af185cfc9c85ccaae32e1f74f87a4e61
checkmate69 <60278795+checkmate69@users.noreply.github.com>
2025-10-26 20:33:03 +0100

    Add files via upload

---
+++ RestorePointMODIF1ED.bat
@@ -0,0 +1,24 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+color 0D
+
+Mode 30,10
+
+:3218321831283128
+
+
+    powershell -ExecutionPolicy Bypass -Command "Checkpoint-Computer -Description 'Before Efficient SysX Optimization' -RestorePointType 'MODIFY_SETTINGS'"
+    
+goto gowno
+:gowno
+
+echo Click enter to exit
+pause>nul
+
+
+
+
+
+
+
+
