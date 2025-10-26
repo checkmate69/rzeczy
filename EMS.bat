1b635d7f029742c82a414b3e716cf09a68f93db6
checkmate69 <60278795+checkmate69@users.noreply.github.com>
2025-10-26 23:59:04 +0100

    Add files via upload

---
+++ EMS.bat
@@ -0,0 +1,49 @@
+color 0D
+
+
+@echo off
+SETLOCAL DISABLEDELAYEDEXTENSIONS
+
+
+Mode 105,30
+
+
+
+
+:1117777
+echo.
+set /p wybor997="Attention! You will not have access to "rescue tools", enter "Disable" or "Revert":   "
+:petla997
+if /i "%wybor997%"=="Disable" goto 1118888
+if /i "%wybor997%"=="Revert" goto 1119999
+echo.
+echo incorrect answer, enter only "Disable" or "Revert".
+echo.
+set /p wybor997="Attention! You will not have access to "rescue tools", enter "Disable" or "Revert":  "
+goto petla997
+
+
+
+:1118888
+cls
+bcdedit /set {current} bootems off
+pause>nul
+cls
+del %0
+
+
+
+:1119999
+cls
+bcdedit /set {current} bootems on
+pause>nul
+cls
+del %0
+
+
+
+
+
+
+
+
